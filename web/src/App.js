3437202c6d877a2606432f5e92dfff8a30e6048b
Mathias L. H <s144229@student.dtu.dk>
2019-09-17 13:59:35 +0200

    second step in automating pipeline

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            it just werks!
+            it just werks! now with autopipe!
         </p>
         <a
           className="App-link"
