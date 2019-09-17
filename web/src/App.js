232e8899b19c620533b144f7c054515fb75caa36
Mathias L. H <s144229@student.dtu.dk>
2019-09-17 15:38:05 +0200

    the fugg

--- web/src/App.js
+++ web/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            it just werks! now with autopipe! really!
+                the final test
         </p>
         <a
           className="App-link"
