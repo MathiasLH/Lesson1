190b9a8dbc96c68a42cc5d1fae3632834e5c3671
Mathias L. H <s144229@student.dtu.dk>
2019-09-17 13:40:06 +0200

    added react

---
+++ src/main/webapp/precache-manifest.61a5059718226056dbade712183058ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b74164cd97d1cb61da3afd5526ae306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "289d01ac5e67550cb40c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e1d9cc27791ac614d15",
+    "url": "/static/js/2.8ce7f97d.chunk.js"
+  },
+  {
+    "revision": "289d01ac5e67550cb40c",
+    "url": "/static/js/main.026c448e.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
